2a76d992b6b2039a4be9ab7b3b88d095ed7ca663
Aaru <aryarai1569@protonmail.com>
2025-01-16 18:53:44 +0530

    config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "20445873"))
 API_HASH = environ.get("API_HASH", "057fd0be9d7c38526b143c582bceb24b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7621787274:AAECIMeEWeVmlMQV0FjDLPg0Spq8UIWWZwQ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7817283258:AAHgo53ZYNPXItvUfp2fKhGVsoM_7cO5apc")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "JUST_TESTING_REBOT") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "@MCAMPUS_File_Bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
